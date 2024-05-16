0f06729a9dcc2b64f932d084ebec37b6987a6315
Racheal Abosede <ajiboyeabosederacheal@gmail.com>
2024-05-16 19:21:01 +0200

    updated reset link page

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,10 +1,12 @@
 import Paginate from "../components/paginate.jsx";
+import ResetLinkPage from "../components/ResetLinkPage.jsx";
 
 function Form() {
     return (
         <div className=" flex justify-center items-center p-1 ">
           <Paginate />
           <div className="h-screen w-1/2">
+          <ResetLinkPage /> 
             
           </div>
         </div>
