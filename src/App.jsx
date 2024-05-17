609f9bfacb4295f32204f493a03eb5d7fcc47296
Victor Onipe <victoronipe565@gmail.com>
2024-05-17 08:36:01 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,1 @@
-// import React from "react";
-//import "../dist/main.css"; comment this out because thr file does not exist..to avoid errors
-import "../src/index.css";
-import Form from "./components/form.jsx";
 
-const App = () => {
-    return (
-        <div>
-        <Form />
-        </div>
-    );
-};
-
-export default App;
